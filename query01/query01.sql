78d96e41588d783cb73b4f39a3b10a26c55349cd
kevinmok2011 <kevinmok2011@gmail.com>
2023-08-01 12:38:21 -0400

    Add SQL query 01

--- query01/query01.sql
+++ query01/query01.sql
@@ -2,4 +2,7 @@ SELECT r.rental_date, r.return_date, c.first_name, c.last_name
 FROM rental r
 INNER JOIN customer c
 on r.customer_id = c.customer_id
-where (r.return_date - r.rental_date) <= INTERVAL '7' DAY;
+where (r.return_date - r.rental_date) <= INTERVAL '7' DAY
+ORDER BY r.return_date;
+
+-- co
